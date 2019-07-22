8917aac286805adc8653c4b01ae8e733fb6a45bf
Phil Goo Kang <philgookang@gmail.com>
2019-07-22 20:51:38 +0900

    fixed pretrain to working with new structure

--- pretrain.py
+++ pretrain.py
@@ -9,33 +9,13 @@ import numpy as np
 import torch.nn as nn
 import visdom
 import torch.backends.cudnn as cudnn
+from torchvision import transforms
 
-def train(dataset, lr):
+def train(pos, dataset, learning_rate, use_visdom):
 
     # #################################################################################
     # VARIABLES
     # #################################################################################
-
-    # learning rate
-    learning_rate = lr
-
-    # size of each training cycle
-    batch_size = 124
-
-    # number of epochs
-    number_epochs = 6
-
-    # number of process worker
-    number_of_workers = 32
-
-    # randomize dataset
-    is_shuffle = True
-
-    # image random crop size
-    crop_size = 224
-
-    # log step size
-    log_step = 36
 
     # GPU or CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -49,6 +29,14 @@ def train(dataset, lr):
     # print embed size
     print("embed_size: ", embed_size)
 
+
+    trans =  transforms.Compose([
+        transforms.RandomCrop(image_crop_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+
+
     # create visdom graph
     vis = visdom.Visdom()
 
@@ -60,9 +48,9 @@ def train(dataset, lr):
     # #################################################################################
 
     # data loader
-    loader = torch.utils.data.DataLoader(dataset=PretrainDataset(dataset=dataset,transform=resnet_transform()),
-                                         batch_size = batch_size,
-                                         shuffle = is_shuffle,
+    loader = torch.utils.data.DataLoader(dataset=PretrainDataset(dataset=dataset,transform=trans),
+                                         batch_size = pretrain_batch_size,
+                                         shuffle = True,
                                          num_workers = number_of_workers,
                                          collate_fn = coco_collate_fn)
 
@@ -88,7 +76,7 @@ def train(dataset, lr):
 
     # train the model
     total_step = len(loader)
-    for epoch in range(number_epochs):
+    for epoch in range(pretrain_number_epochs):
         for i, (images, labels, lengths) in enumerate(loader):
 
             # set tensor to GPU
@@ -110,22 +98,19 @@ def train(dataset, lr):
             loss_val = loss.item()
 
             # check for every step
-            if i % log_step == 0:
+            if i % 36 == 0:
 
                 # graph loss and process
                 if use_visdom:
                     if loss_graph == None:
-                        legend = ["Epoch {0}".format(epoch) for epoch in range(number_epochs)]
+                        legend = ["Epoch {0}".format(epoch) for epoch in range(pretrain_number_epochs)]
                         loss_graph = vis.line(X=np.array([i]), Y=np.array([loss_val]), name="Epoch {0}".format(epoch), opts=dict(
-                                                  title = "{0} (LR: {1})".format(book.target.capitalize(), learning_rate), legend=legend, xlabel='Iteration', ylabel='Loss'
+                                                  title = "{0} (LR: {1})".format(pos, learning_rate), legend=legend, xlabel='Iteration', ylabel='Loss'
                                               ))
                     else:
                         vis.line(X=np.array([i]), Y=np.array([loss_val]), name="Epoch {0}".format(epoch), update='append', win=loss_graph)
 
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, number_epochs, i+1, total_step, loss_val))
-
-            break
-        break
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, pretrain_number_epochs, i+1, total_step, loss_val))
 
     # return trained model
     return cnn_model
@@ -142,13 +127,13 @@ if __name__ == "__main__":
     cudnn.benchmark = True
 
     # loop through list
-    for pos in ["adjective"]: #"noun", "verb",  , "conjunction", "preposition"
+    for pos in ["adjective", "noun", "verb", "conjunction", "preposition"]:
 
         # loop through each different learning rate
         for lr in learning_rate:
 
             # train PoS CNN
-            trained_model = train(dataset[pos], lr)
+            trained_model = train(pos, dataset[pos], lr, True)
 
             # save trained model
             save_model(trained_model, model_file[pos]["pretrain"])

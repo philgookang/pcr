4e2401476b2c65d86366ca2ff4ca63fb4f7ff2ee
Phil Goo Kang <philgookang@gmail.com>
2019-07-22 22:15:57 +0900

    removed old code, updated for new dataset

--- ready_dataset.py
+++ ready_dataset.py
@@ -2,49 +2,42 @@ from helper import *
 from component import *
 from config import *
 
-from pycocotools.coco import COCO
 from tqdm import tqdm
 from collections import Counter
 
 import nltk
 import pickle
+import json
 
 # coco initalize
-coco = COCO(coco_caption_path)
-ids = coco.anns.keys()
+with open(coco_caption_path_train) as f:
+    train_coco = json.load(f)
+
+# coco image holder
+image_holder = { }
+for item in train_coco["images"]:
+    image_holder[item["id"]] = item
 
 # ###################################################
 print("load complete dataset")
 # ###################################################
 
 # complete_dataset
-complete_dataset = { }
+train_set = { }
 
 # this list holds { "filename" : {"id", "caption"} }
-for i, id in tqdm(enumerate(ids)):
+for item in tqdm(train_coco["annotations"]):
 
     # retrieve item information
-    caption = str(coco.anns[id]["caption"])
-    filename = coco.loadImgs(coco.anns[id]["image_id"])[0]["file_name"]
+    caption = str(item["caption"])
+    filename = image_holder[item["image_id"]]["file_name"]
 
     # check if already added
-    if filename not in complete_dataset:
-        complete_dataset[filename] = { "id" : [], "caption" : [], "image_id" : [] }
+    if filename not in train_set:
+        train_set[filename] = { "id" : [], "caption" : [], "image_id" : [] }
 
-    complete_dataset[filename]["id"].append(id)
-    complete_dataset[filename]["caption"].append(caption)
-    complete_dataset[filename]["image_id"].append(coco.anns[id]["image_id"])
-
-
-# ###################################################
-print("parse dataset by train, validation, test")
-# ###################################################
-
-train_set, validation_set, test_set = prase_data_by_ratio(complete_dataset, 5000)
-
-save_dataset(dataset_file["train_org"], train_set)
-save_dataset(dataset_file["validation_org"], validation_set)
-save_dataset(dataset_file["test_org"], test_set)
+    train_set[filename]["id"].append(id)
+    train_set[filename]["caption"].append(caption)
 
 
 # ###################################################
@@ -96,41 +89,6 @@ words = [word for word, cnt in counter.items() if cnt >= threshold]
 # loop through words that pass threshold
 for word in words: train_dataset["corpus"].append(word)
 
+# save dataset to file
 save_dataset(dataset_file["pretrain"], pretrain_dataset)
 save_dataset(dataset_file["train"], train_dataset)
-
-
-# ###################################################
-print("validation & test dataset")
-# ###################################################
-
-validation_dataset = {
-    "corpus" : ["<pad>", "<start>", "<end>", "<unk>"], # complete list of all the words used
-    "data" : [] # { "filename" : "", "caption" : "" }
-}
-
-test_dataset = {
-    "corpus" : ["<pad>", "<start>", "<end>", "<unk>"], # complete list of all the words used
-    "data" : [] # { "filename" : "", "caption" : "" }
-}
-
-for filename in tqdm(validation_set):
-    item = validation_set[filename]
-    for id, caption in tqdm(zip(item["id"], item["caption"])):
-        tokens = nltk.tokenize.word_tokenize(caption.lower())
-        validation_dataset["data"].append({ "filename" : filename, "caption" : tokens })
-        for word in tokens:
-            if word not in validation_dataset["corpus"]:
-                validation_dataset["corpus"].append(word)
-
-for filename in tqdm(test_set):
-    item = test_set[filename]
-    for id, caption in tqdm(zip(item["id"], item["caption"])):
-        tokens = nltk.tokenize.word_tokenize(caption.lower())
-        test_dataset["data"].append({ "filename" : filename, "caption" : tokens })
-        for word in tokens:
-            if word not in test_dataset["corpus"]:
-                test_dataset["corpus"].append(word)
-
-save_dataset(dataset_file["validation"], validation_dataset)
-save_dataset(dataset_file["test"], test_dataset)

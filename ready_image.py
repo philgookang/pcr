1c847bcda8960ff02672bd5ae1626fee8edb3863
Phil Goo Kang <philgookang@gmail.com>
2019-07-22 22:25:04 +0900

    added resize image code

---
+++ ready_image.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+from PIL import Image
+from tqdm import tqdm
+
+def resize_image(image, size):
+    """Resize an image to the given size."""
+    return image.resize(size, Image.ANTIALIAS)
+
+def resize_images(image_dir, output_dir, size):
+    """Resize the images in 'image_dir' and save into 'output_dir'."""
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    images = os.listdir(image_dir)
+    num_images = len(images)
+    for i, image in enumerate(images):
+        with open(os.path.join(image_dir, image), 'r+b') as f:
+            with Image.open(f) as img:
+                img = resize_image(img, size)
+                img.save(os.path.join(output_dir, image), img.format)
+        if (i+1) % 100 == 0:
+            print ("[{}/{}] Resized the images and saved into '{}'."
+                   .format(i+1, num_images, output_dir))
+
+if __name__ == '__main__':
+
+    image_size = 256
+    target_folders = ["../data/val2017/"] #, "../data/test2017/", "../data/train2017/"
+    result_folders = ["../data/re_val2017/"] #, "../data/re_test2017/", "../data/re_train2017/"
+
+    for target, result in tqdm(zip(target_folders, result_folders)):
+        resize_images(target, result, [image_size, image_size])

45519aa2f087ea3bd762764c16414d1e90c65f36
Phil Goo Kang <philgookang@gmail.com>
2019-07-22 22:15:34 +0900

    removed unsed function

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,4 +1,3 @@
-from helper.helper_parse import prase_data_by_ratio
 from helper.helper_torch import coco_collate_fn
 from helper.helper_file import save_dataset
 from helper.helper_file import load_dataset

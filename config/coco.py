d6f77943f521aabf9647715ee4b611e09449f608
Phil Goo Kang <philgookang@gmail.com>
2019-07-23 17:58:35 +0900

    Changed path maker to support multios

--- config/coco.py
+++ config/coco.py
@@ -1,7 +1,7 @@
 import os
 
 base_path = os.getcwd()
-coco_data_path = base_path + "/../data/"
-coco_caption_path_train = coco_data_path + "annotations/captions_train2017.json"
-coco_image_path = coco_data_path + "resized2014/"
-coco_pretrain_image_path = coco_data_path + "re_train2017/"
+coco_data_path = os.path.join(base_path, "..", "data", "")
+coco_caption_path_train = os.path.join(coco_data_path, "annotations", "captions_train2017.json")
+coco_image_path = os.path.join(coco_data_path, "resized2014", "")
+coco_pretrain_image_path = os.path.join(coco_data_path, "re_train2017", "")

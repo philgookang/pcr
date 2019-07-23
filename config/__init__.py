fb74411b974d63f402755de3a70354d7aa68aa66
Phil Goo Kang <philgookang@gmail.com>
2019-07-23 18:59:43 +0900

    added base path

--- config/__init__.py
+++ config/__init__.py
@@ -10,7 +10,7 @@ from config.train import number_of_workers
 from config.train import pretrain_number_epochs
 from config.train import pretrain_batch_size
 
-
+from config.coco import base_path
 from config.coco import coco_data_path
 from config.coco import coco_caption_path_train
 from config.coco import coco_image_path ### easier later on

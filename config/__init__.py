a710567684d892505fe1cf16ec686fe753f275e7
Phil Goo Kang <philgookang@gmail.com>
2019-07-24 17:14:05 +0900

    added train batch size and train image path variable

--- config/__init__.py
+++ config/__init__.py
@@ -9,9 +9,10 @@ from config.train import image_crop_size
 from config.train import number_of_workers
 from config.train import pretrain_number_epochs
 from config.train import pretrain_batch_size
+from config.train import train_batch_size
 
 from config.coco import base_path
 from config.coco import coco_data_path
 from config.coco import coco_caption_path_train
-from config.coco import coco_image_path ### easier later on
+from config.coco import coco_train_image_path
 from config.coco import coco_pretrain_image_path

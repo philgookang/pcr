3b0cad6e2284c0ee85f1bcd73278e7fec2843713
Phil Goo Kang <philgookang@gmail.com>
2019-07-25 13:44:36 +0900

    removed model saving code

--- component/early_stopping.py
+++ component/early_stopping.py
@@ -8,7 +8,7 @@ class EarlyStopping:
         Args:
             patience (int): How long to wait after last time validation loss improved.
                             Default: 7
-            verbose (bool): If True, prints a message for each validation loss improvement. 
+            verbose (bool): If True, prints a message for each validation loss improvement.
                             Default: False
         """
         self.patience = patience
@@ -16,15 +16,13 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
 
-    def __call__(self, val_loss, model):
+    def __call__(self, val_loss):
 
         score = -val_loss
 
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
         elif score < self.best_score:
             self.counter += 1
             # print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -32,12 +30,4 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
             self.counter = 0
-
-    def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
-        if self.verbose:
-            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), 'checkpoint.pt')
-        self.val_loss_min = val_loss

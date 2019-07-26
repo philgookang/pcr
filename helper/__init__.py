c9293b586180134fc0df177c8f7a381ac972d864
Phil Goo Kang <philgookang@gmail.com>
2019-07-26 16:10:29 +0900

    changed data parser for train/test code

--- helper/__init__.py
+++ helper/__init__.py
@@ -4,3 +4,5 @@ from helper.helper_torch import combine_vertically
 from helper.helper_file import save_dataset
 from helper.helper_file import load_dataset
 from helper.helper_file import save_model
+
+from helper.helper_parse import prase_data_by_ratio

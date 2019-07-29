1b0d88cafb28705e07ae9bcde55c25eddfc5ff00
Phil Goo Kang <philgookang@gmail.com>
2019-07-29 19:09:53 +0900

    added captoin image generator function

--- helper/__init__.py
+++ helper/__init__.py
@@ -4,5 +4,6 @@ from helper.helper_torch import combine_vertically
 from helper.helper_file import save_dataset
 from helper.helper_file import load_dataset
 from helper.helper_file import save_model
+from helper.helper_file import create_image_caption
 
 from helper.helper_parse import prase_data_by_ratio
--- helper/helper_file.py
+++ helper/helper_file.py
@@ -1,6 +1,7 @@
 import torch
 import pickle
 import os
+from PIL import Image, PSDraw
 from config import *
 
 def save_dataset(filename, target):
@@ -13,3 +14,17 @@ def load_dataset(filename):
 
 def save_model(model, filename):
     torch.save(model.state_dict(), os.path.join(base_path, "result", "model", filename))
+
+def create_image_caption(original, target, lst):
+
+    font = os.path.join(base_path, "config", "RobotoRegular.ttf")
+
+    img = Image.open(original, 'r')
+    w, h = img.size
+    img = img.crop((0,0,w + 900,h))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font, 24)
+#     text_w, text_h = draw.textsize(lst[0]) #, font
+    for no,txt in enumerate(lst):
+        draw.text((w + 50, 100 + (50*no)), txt, (0,0,0), font=font)
+    img.save(target)

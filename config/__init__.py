8b811cf0e93aae6cd6c42c71c2a18fd4f5ee2681
Phil Goo Kang <philgookang@gmail.com>
2019-08-05 12:48:00 +0900

    added heatmap config

--- config/__init__.py
+++ config/__init__.py
@@ -21,9 +21,12 @@ from config.coco import coco_validation_image_path
 from config.coco import coco_test_image_path
 from config.coco import coco_test_image_path_original
 from config.coco import coco_pretrain_image_path
+from config.coco import coco_pretrain_image_path_original
 from config.coco import train_early_stop
 from config.coco import model_save_path
 from config.coco import image_with_caption
+from config.coco import heatmap_path
+from config.coco import heatmap_cnn_path
 
 from config.rnn import decoder_hidden_size
 from config.rnn import lstm_number_of_layers

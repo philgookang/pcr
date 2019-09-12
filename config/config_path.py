98687c77d57bb3fd8353d5895f44084844714b4d
Phil Goo Kang <philgookang@gmail.com>
2019-09-12 23:19:39 +0900

    added flick30k image path

--- config/config_path.py
+++ config/config_path.py
@@ -15,6 +15,7 @@ FLICKR8k_VALIDATION_IMG = os.path.join(COCO_ROOT, "Flickr_8k.devImages.txt")
 FLICKR8k_TEST_IMG = os.path.join(COCO_ROOT, "Flickr_8k.testImages.txt")
 FLICKR8k_ANNOTATION = os.path.join(COCO_ROOT, "Flickr8k.token.txt")
 
+FLICKR30k_IMAGE_PATH = os.path.join(COCO_ROOT, "re_flickr30k", "")
 FLICKR30k_ANNOTATION = os.path.join(COCO_ROOT, "flickr30k.txt")
 
 RESULT_IMAGE_W_CAPTION = os.path.join(RESULT_ROOT, "image_with_caption", "")
@@ -27,3 +28,5 @@ RESULT_DATASET_PATH = os.path.join(RESULT_ROOT, "dataset", "")
 IMG_PATH = COCO_IMAGE_PATH # default 1
 if dataset_type == 2:
     IMG_PATH = FLICKR8k_IMAGE_PATH
+elif dataset_type == 3:
+    IMG_PATH = FLICKR30k_IMAGE_PATH

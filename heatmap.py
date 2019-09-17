94945647d91e7a7390d1c1d1216d1ffdbfe94d31
Phil Goo Kang <philgookang@gmail.com>
2019-09-17 21:35:46 +0900

    added heatmap for model MSCOCO

--- heatmap.py
+++ heatmap.py
@@ -17,21 +17,12 @@ from tqdm import tqdm
 
 from PIL import Image
 
-def heatmap_cnn_image(filename, file_name_to_export, device, pos, target_class, target_layer, dataset):
+import sys
+
+def heatmap_cnn_image(cnn_model, filename, file_name_to_export, device, pos, target_class, target_layer, dataset):
 
     # load image
     image = Image.open(filename).convert('RGB')
-
-    features = torch.load(RESULT_MODEL_PATH + model_file[pos]["train"])
-    cnn_model = Encoder(embed_size = 1024)
-    cnn_model = nn.DataParallel(cnn_model)
-    cnn_model.to(device, non_blocking=True)
-    cnn_model.load_state_dict(features)
-    cnn_model.eval() # change model mode to eval
-
-    cnn_model = cnn_model.module
-    cnn_model.update_layer(len(dataset["corpus"]))
-    cnn_model.eval()
 
     # Grad cam
     grad_cam = GradCam(cnn_model, target_layer = target_layer)
@@ -45,39 +36,92 @@ def heatmap_cnn_image(filename, file_name_to_export, device, pos, target_class, 
     # save result
     grad_cam.save_class_activation_images(image, cam, file_name_to_export)
 
+def load_image_file(filename_with_full_path, trans, device):
+    image  = Image.open(filename_with_full_path)
+    image = image.resize([224, 224], Image.LANCZOS)
+    image = trans(image).unsqueeze(0)
+    image = image.to(device, non_blocking = True)
+    return image
+
 if __name__ == "__main__":
 
-    dataset = load_dataset(dataset_file["pretrain"])
-    trans =  transforms.Compose([transforms.RandomCrop(image_crop_size),transforms.RandomHorizontalFlip(),transforms.ToTensor(),transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    target_layer = 4
+    if len(sys.argv) >= 2:
 
-    for pos in ["verb", "adjective", "conjunction", "preposition"]:
+        # get filename
+        type = sys.argv[1]
 
-        pretrain_dataset = PretrainDataset(dataset = dataset[pos], transform = trans, img_path = IMG_PATH)
+        print("type", type)
 
-        for i in tqdm(range(len(pretrain_dataset))):
+        test_dataset = load_dataset(dataset_file["test"])
+        dataset = load_dataset(dataset_file["pretrain"])
 
-            # get item
-            item = pretrain_dataset.get(i)
+        trans =  transforms.Compose([transforms.RandomCrop(image_crop_size),transforms.RandomHorizontalFlip(),transforms.ToTensor(),transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
-            # get label
-            label = pretrain_dataset.convert_word(item["word"])
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        target_layer = 4
 
-            # create file name
-            file_split = item["filename"].split('.')
-            filename = "{0}_{1}.{2}".format(i, item["word"], file_split[1])
+        # "verb" 0, "adjective" 1, "conjunction" 2, "preposition" 3
+        # pos_name    = ""
+        # gpu_num     = None
+        #
+        # if type == '1':
+        #     pos_name = "verb"
+        #     gpu_num = 0
+        # elif type == '2':
+        #     pos_name = "adjective"
+        #     gpu_num = 1
+        # elif type == '3':
+        #     pos_name = "conjunction"
+        #     gpu_num = 2
+        # elif type == '4':
+        #     pos_name = "preposition"
+        #     gpu_num = 3
+        #
+        # print("pos_name", pos_name)
 
-            # get image
-            heatmap_org_image_result_path = os.path.join(HEATMAP_CNN, pos, filename.replace(".", "_o.") )
-            heatmap_result_path = os.path.join(HEATMAP_CNN, pos, filename )
+        for pos in ["verb", "adjective", "conjunction", "preposition"]:
 
-            copyfile(IMG_PATH + item["filename"], heatmap_org_image_result_path)
-            heatmap_cnn_image(IMG_PATH + item["filename"], heatmap_result_path, device, pos, label, target_layer, dataset[pos])
+            pretrain_dataset = PretrainDataset(dataset = dataset[pos], transform = trans, img_path = IMG_PATH)
 
-            break
-        break
+            features = torch.load(RESULT_MODEL_PATH + model_file[pos]["train"])
+            cnn_model = Encoder(embed_size = 1024)
+            cnn_model = nn.DataParallel(cnn_model) # , device_ids = [gpu_num]
+            cnn_model.to(device, non_blocking=True)
+            cnn_model.load_state_dict(features)
+            cnn_model.eval() # change model mode to eval
 
+            cnn_model = cnn_model.module
+            cnn_model.update_layer(len(dataset[pos]["corpus"]))
+            cnn_model.eval()
+            cnn_model.to(device, non_blocking=True)
+
+            i = 0
+            for filename in tqdm(test_dataset):
+
+                image = load_image_file(IMG_PATH + filename, trans, device)
+
+                features = None
+                try:
+                    features = cnn_model(image)
+                except RuntimeError:
+                    continue
+
+                word = pretrain_dataset.decode_word(features)
+                label = pretrain_dataset.convert_word(word)
+
+                # create file name
+                file_split = filename.split('.')
+                newfilename = "{0}_{1}.{2}".format(i, word, file_split[1])
+
+                # get image
+                heatmap_org_image_result_path = os.path.join(HEATMAP_CNN, pos, newfilename.replace(".", "_o.") )
+                heatmap_result_path = os.path.join(HEATMAP_CNN, pos, newfilename )
+
+                copyfile(IMG_PATH + filename, heatmap_org_image_result_path)
+                heatmap_cnn_image(cnn_model, IMG_PATH + filename, heatmap_result_path, device, pos, label, target_layer, dataset[pos])
+
+
+                i += 1
 
 
 

9c32816d66bd5e40fdd9fc2eb7f1b1c9897ac5c6
Phil Goo Kang <philgookang@gmail.com>
2019-10-02 13:49:35 +0900

    removed functions not need for release

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,8 +1,5 @@
 from helper.helper_torch import coco_collate_fn
-from helper.helper_torch import coco_collate_fn_for_bidirectional
-from helper.helper_torch import combine_vertically
 from helper.helper_torch import combine_output
-from helper.helper_torch import flip_tensor
 
 from helper.helper_file import save_dataset
 from helper.helper_file import load_dataset
--- helper/helper_file.py
+++ helper/helper_file.py
@@ -16,15 +16,12 @@ def save_model(model, filename):
     torch.save(model.state_dict(), os.path.join(RESULT_MODEL_PATH, filename))
 
 def create_image_caption(original, target, lst):
-
     font = os.path.join(BASE_PATH, "rss", "RobotoRegular.ttf")
-
     img = Image.open(original, 'r')
     w, h = img.size
     img = img.crop((0,0,w + 900,h))
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(font, 20)
-#     text_w, text_h = draw.textsize(lst[0]) #, font
     for no,txt in enumerate(lst):
         draw.text((w + 10, 2 + (37*no)), txt, (255,255,255), font=font)
     img.save(target)

64b80215898e073b41cc35f79890f990540fba35
Phil Goo Kang <philgookang@gmail.com>
2019-12-19 10:37:24 +0900

    updated BLEU with ngram weights

--- evaluation/bleu.py
+++ evaluation/bleu.py
@@ -7,16 +7,16 @@ class Bleu():
         pass
 
     def compute_wrapper_1(self, x):
-        return self.compute_score(x["reference"], x["hypothesis"], SmoothingFunction().method1)
+        return self.compute_score(x["reference"], x["hypothesis"], (1, 0, 0, 0),  SmoothingFunction().method2)
 
     def compute_wrapper_2(self, x):
-        return self.compute_score(x["reference"], x["hypothesis"], SmoothingFunction().method2)
+        return self.compute_score(x["reference"], x["hypothesis"], (0.5, 0.5, 0, 0),  SmoothingFunction().method2)
 
     def compute_wrapper_3(self, x):
-        return self.compute_score(x["reference"], x["hypothesis"], SmoothingFunction().method3)
+        return self.compute_score(x["reference"], x["hypothesis"], (0.33, 0.33, 0.33, 0), SmoothingFunction().method2)
 
     def compute_wrapper_4(self, x):
-        return self.compute_score(x["reference"], x["hypothesis"], SmoothingFunction().method4)
+        return self.compute_score(x["reference"], x["hypothesis"], (0.25, 0.25, 0.25, 0.25), SmoothingFunction().method2)
 
     def compute(self, dataset):
         '''
@@ -36,8 +36,9 @@ class Bleu():
             s4 = list(p.map(self.compute_wrapper_4, dataset))
         return (sum(s1)/len(s1)), (sum(s2)/len(s2)), (sum(s3)/len(s3)), (sum(s4)/len(s4))
 
-    def compute_score(self, reference, hypothesis, smoothing_function = SmoothingFunction().method0):
-        return sentence_bleu(reference, hypothesis, smoothing_function = smoothing_function)
+    def compute_score(self, reference, hypothesis, ww, smoothing_function = SmoothingFunction().method0):
+        # manual changes to weight value for different BLEU scores
+        return sentence_bleu(reference, hypothesis, weights=ww, smoothing_function = smoothing_function)
 
 if __name__ == "__main__":
     b = Bleu()
